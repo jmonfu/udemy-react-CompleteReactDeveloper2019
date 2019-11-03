3526cc4fb864716e126611172c7556d8f169a315
Johann Montfort <jmonfu@gmail.com>
2019-11-03 16:48:10 +0100

    Updates

---
+++ precache-manifest.46f028228f1d3a662e14de5f819fc06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc6782623171d5e6cdc3e5835d23dac",
+    "url": "/udemy-react-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f89b9493fba5b3ae8e27",
+    "url": "/udemy-react-Monsters-Rolodex/static/css/main.25375ba3.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/udemy-react-Monsters-Rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "f89b9493fba5b3ae8e27",
+    "url": "/udemy-react-Monsters-Rolodex/static/js/main.928f4239.chunk.js"
+  },
+  {
+    "revision": "46506ecd93455825401e",
+    "url": "/udemy-react-Monsters-Rolodex/static/js/runtime-main.6cacab36.js"
+  }
+]);
